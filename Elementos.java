69488ea248a601678bc8f3b9b72d9ed5dd51c86a
joseluistapia26 <tapiasoftware@gmail.com>
2026-01-26 21:42:31 -0500

    Add files via upload
    Clase con elementos Estaticos

---
+++ Elementos.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package lives2026;
+
+/**
+ *
+ * @author Usuario
+ */
+public class Elementos {
+    public static final int SIGLO=100;
+    public static double valor1;
+    
+    public static void imprimir(String msg){
+        System.out.println(msg);
+    }
+    
+    public static double suma(double x, double y){
+        return (x+y);
+    }
+    
+    public static double resta(double x, double y){
+        return (x-y);
+    }
+}
