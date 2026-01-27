90046578889b0ef372440e0aa30ef39afd7e9fba
joseluistapia26 <tapiasoftware@gmail.com>
2026-01-26 21:40:41 -0500

    Refactor Lives2026 class and add new methods
    Elementos Estaticos

--- Lives2026.java
+++ Lives2026.java
@@ -1,3 +1,6 @@
+package lives2026;
+
+import java.util.Scanner;
 import javax.swing.JOptionPane;
 
 /**
@@ -6,9 +9,11 @@ import javax.swing.JOptionPane;
  */
 public class Lives2026 {
    static int valor;
+   static String palabra;
+   
     
     public static void main(String[] args) {
-        ejemplo3();
+        ejemplo5();
     }
     
     private static void prueba(){
@@ -49,5 +54,31 @@ public class Lives2026 {
         System.out.println("Valor "+200);
     }
     
+    private static void ejemplo4(){
+        final double PI = 3.14;
+        palabra = "Jose";
+        int edad;
+        var valor = 0;
+        Scanner lec = new Scanner(System.in);
+        System.out.print("Ingrese su edad:");
+        edad = lec.nextInt();
+        System.out.println("PI:"+PI);
+    }
+    
+    private static void ejemplo5(){
+        System.out.println("Constante:"+Elementos.SIGLO);
+        Scanner lec = new Scanner(System.in);
+        System.out.print("Ingresa un valor:");
+        Elementos.valor1 = lec.nextDouble();
+        Elementos.imprimir("El valor ingresado es:"+Elementos.valor1);
+        double v1,r;
+        var v2=0.0;
+        System.out.print("Numero 1:");
+        v1 = lec.nextDouble();
+        System.out.print("Numero 2:");
+        v2 = lec.nextDouble();
+        r = Elementos.suma(v1, v2);
+        Elementos.imprimir("La suma es:"+r);
+    }
+    
 }
-
