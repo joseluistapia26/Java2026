d711ed7564f550a0c7bc96cee5c64f8417d5565a
joseluistapia26 <tapiasoftware@gmail.com>
2026-01-29 21:38:16 -0500

    Condicionales en Java

---
+++ Condicionales.java
@@ -0,0 +1,81 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package lives2026;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Jose Luis
+ * @Date 29-01-2026
+ */
+public class Condicionales {
+
+    public static void main(String[] args) {
+        eje4();
+    }
+
+    private static void eje1() {
+        Scanner lec = new Scanner(System.in);
+        int codigo;
+        System.out.print("Ingrese un codigo:");
+        String dato = lec.nextLine();
+        codigo = Integer.parseInt(dato);
+        switch (codigo) {
+            case 200, 201:
+                System.out.println("Exito: Solicitud procesada.");
+                break;
+            case 400, 404:
+                System.out.println("Error detectado!");
+                break;
+            default:
+                System.out.println("Codigo no existe!!");
+        }
+    }
+
+    private static void eje2() {
+        Scanner lec = new Scanner(System.in);
+        int codigo;
+        System.out.print("Codigo:");
+        String dato = lec.nextLine();
+        codigo = Integer.parseInt(dato);
+        String msg = switch (codigo) {
+            case 200, 201 ->
+                "Exito: Solicitud procesada";
+            case 400, 404 ->
+                "Error detectado!";
+            default ->
+                "Estado desconocido";
+        };
+        System.out.println(msg);
+    }
+
+    private static void eje3() {
+        int edad;
+        Scanner lec = new Scanner(System.in);
+        System.out.print("Edad:");
+        edad = lec.nextInt();
+        if (edad > 17) {
+            System.out.println("Mayor de edad!");
+        } else {
+            System.out.println("Menor de edad!");
+        }
+    }
+
+    private static void eje4() {
+        String nombre;
+        Scanner lec = new Scanner(System.in);
+        System.out.print("Nombre:");
+        nombre = lec.nextLine().toUpperCase();
+        System.out.println(nombre);
+        if ("ANA".equals(nombre) || "MARIA".equals(nombre)
+                   || "CARLOS".equals(nombre)) {
+            System.out.println("es de la ciudad");
+        }else{
+             System.out.println("Es del Campo");
+        }
+    }
+
+}
